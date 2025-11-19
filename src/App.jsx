b6cc3eeacd079ec8236b4bc375962be8ccee29a2
mozta <perezaguirrerafael@gmail.com>
2025-11-19 16:13:47 -0600

    base code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-500">Base SAAS</h1>
+      <p className="text-lg">
+
+        
+        Sistema base con autenticación, roles de usuario y rutas protegidas
+      </p>
+    </>
+  );
+}
+
+export default App;
